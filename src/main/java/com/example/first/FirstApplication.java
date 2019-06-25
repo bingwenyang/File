6dbc2cadfc7e5868958fac9276e5618b8a486bdd
YangBingWen <470926176@qq.com>
2019-06-25 17:09:54 +0800

    1

---
+++ src/main/java/com/example/first/FirstApplication.java
@@ -0,0 +1,24 @@
+package com.example.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+
+
+
+public class FirstApplication {
+
+    @RequestMapping("/hello")
+    String index(){
+
+        return "hello Spring Boot";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(FirstApplication.class, args);
+    }
+
+}
