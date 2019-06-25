11c248c66375c4772236bd0bfb1b7a8ce9cc9060
bingwenyang <470926176@qq.com>
2019-06-25 17:18:32 +0800

    Update FirstApplication.java

--- src/main/java/com/example/first/FirstApplication.java
+++ src/main/java/com/example/first/FirstApplication.java
@@ -15,7 +15,7 @@ public class FirstApplication {
     @RequestMapping("/")
     String index(){
 
-        return "hello Spring Boot";
+        return "hello Spring Boot Change";
     }
     public static void main(String[] args) {
         SpringApplication.run(FirstApplication.class, args);
